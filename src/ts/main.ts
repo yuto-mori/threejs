9193248fc7845f88fc4fecd97f92797ad5a5845c
yuto mori <yutomori1979@gmail.com>
2020-04-25 22:34:03 +0900

    メッシュの色を変更

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -18,7 +18,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // 箱を作成
   const sphereGeometry = new THREE.SphereGeometry(250, 10, 10);
   const sphereMaterial = new THREE.MeshBasicMaterial({
-    color: 0x0e7529,
+    color: 0xffffff,
     wireframe: true,
   });
   const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
