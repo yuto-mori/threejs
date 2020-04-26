3df916d230c5975bb7902e49e6e246cfcc2865ea
yuto mori <yutomori1979@gmail.com>
2020-04-26 09:33:25 +0900

    ディレクトリ 変更

--- webpack.common.js
+++ webpack.common.js
@@ -1,24 +1,24 @@
 //webpack参考
 //https://ics.media/entry/12140/
 module.exports = {
-    // メインとなるJavaScriptファイル（エントリーポイント）
-    entry: './src/ts/main.ts',
-    module: {
-        rules: [
-            {
-                // 拡張子 .ts の場合
-                test: /\.ts$/,
-                // TypeScript をコンパイルする
-                use: 'ts-loader'
-            }
-        ]
-    },
-    // import 文で .ts ファイルを解決するため
-    resolve: {
-        extensions: ['.ts']
-    },
-    output: {
-        //  出力ファイルのディレクトリ名
-        path: `${__dirname}/project/threejs/assets/js`
-    }
+  // メインとなるJavaScriptファイル（エントリーポイント）
+  entry: './src/ts/main.ts',
+  module: {
+    rules: [
+      {
+        // 拡張子 .ts の場合
+        test: /\.ts$/,
+        // TypeScript をコンパイルする
+        use: 'ts-loader',
+      },
+    ],
+  },
+  // import 文で .ts ファイルを解決するため
+  resolve: {
+    extensions: ['.ts'],
+  },
+  output: {
+    //  出力ファイルのディレクトリ名
+    path: `${__dirname}/dist/assets/js`,
+  },
 };
